3e02dd169b83b880a1c28c7143ecf45f411eb8a1
unknown <fadychorfi05@gmail.com>
2025-04-17 23:48:03 +0100

    complete sign in ui

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -6,6 +6,10 @@ import { Label } from "@/components/ui/label";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import { url } from "inspector";
+import Logo from "../../../../public/logo";
+import Image from "next/image";
+import LogoLight from "../../../../public/logoLight";
 
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const supabase = await createClient();
@@ -13,7 +17,6 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
     data: { session },
   } = await supabase.auth.getSession();
 
-  // Redirect to dashboard if user is already logged in
   if (session) {
     redirect("/protected");
   }
@@ -21,42 +24,111 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen min-w-screen">
-      <form className="flex-1 flex flex-col min-w-64">
-        <h1 className="text-2xl font-medium">Sign in</h1>
-        <p className="text-sm text-foreground">
-          Don't have an account?{" "}
-          <Link
-            className="text-foreground font-medium underline"
-            href="/sign-up"
-          >
-            Sign up
-          </Link>
-        </p>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <div className="flex justify-between items-center">
-            <Label htmlFor="password">Password</Label>
-            <Link
-              className="text-xs text-foreground underline"
-              href="/forgot-password"
+    <main className="flex items-center justify-center min-h-screen bg-white">
+      <div className="w-full max-w-[83rem] flex rounded-xl overflow-hidden ">
+        {/* Left side - Login form */}
+        <div className="w-1/2 p-12">
+          <form className="max-w-md mx-auto  mt-16">
+            <h1 className="text-[2.2rem] font-bold">
+              <span className="text-[#084B54]">Welcome </span>
+              <span className="text-[#0AA462]">back!</span>
+            </h1>
+            <p className="text-sm text-[#084B54] tracking-[-0.025em] font-aeonik mb-4">
+              Doctor or patient? Sign in to continue.
+            </p>
+
+            <div className="flex flex-col gap-1 mb-6">
+              <Label htmlFor="email" className="text-[#084B54] text-[0.87rem] font-aeonik font-normal">
+                Email
+              </Label>
+              <Input
+                name="email"
+                placeholder="you@example.com"
+                required
+                className="bg-white text-black border-[#ADADAD] h-12"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2 mb-6">
+              <div className="flex justify-between items-center">
+                <Label htmlFor="password" className="text-[#084B54] text-[0.87rem] font-aeonik font-normal">
+                  Password
+                </Label>
+                <Link
+                  href="/forgot-password"
+                  className="text-[#555555] text-[0.87rem] tracking-[-0.025em] font-aeonik font-medium"
+                >
+                  Forgot Password?
+                </Link>
+              </div>
+              <Input
+                type="password"
+                name="password"
+                placeholder="••••••••••••"
+                required
+                className="bg-white text-black border-[#ADADAD] h-12"
+              />
+            </div>
+
+            <SubmitButton
+              pendingText="Signing In..."
+              formAction={signInAction}
+              className="bg-[#20504B] font-aeonik w-full h-12 text-white rounded-md"
             >
-              Forgot Password?
-            </Link>
-          </div>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            required
-          />
-          <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-            Sign in
-          </SubmitButton>
-          <FormMessage message={searchParams} />
+              Sign In
+            </SubmitButton>
+
+            <FormMessage message={searchParams} />
+
+            <div className="text-center mt-6">
+              <p className="text-[#555555] text-sm font-aeonik font-normal">
+                Don't have an account?{" "}
+                <Link href="/signup" className="text-[#0AA462] font-medium">
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+
+          </form>
+            <div className="text-center mt-8   ">
+              <p className="text-[#555555] text-xs">
+                By continuing, you agree to our{" "}
+                <Link href="/terms" className="underline">
+                  Terms
+                </Link>{" "}
+                and{" "}
+                <Link href="/privacy" className="underline">
+                  Privacy Policy
+                </Link>
+                ,<br />
+                and that cookies collection is used to improve your experience.
+              </p>
+            </div>
         </div>
-      </form>
+
+        <div className="w-1/2 bg-[#20504B] p-12 flex rounded-3xl flex-col gap-36" style={{ backgroundImage: "url('/auth.svg')" }}>
+          <div className="mb-16">
+            <LogoLight />
+          </div>
+
+          <div className="mt-auto">
+            <p className="text-[1.7rem] font-aeonik font-normal text-white tracking-tight mb-8">
+              "I found a specialist and booked an appointment in under 2 minutes. The process was smooth, clear, and
+              stress-free!"
+            </p>
+
+            <div className="flex items-center gap-4">
+              <Image src="/Avatar.png" width={50} height={50} alt="avatar" className="rounded-full" />
+              <div>
+                <h3 className="font-aeonik font-medium text-[1.4rem] text-white tracking-tight">Dr. Belkacem</h3>
+                <h5 className="text-[0.75rem] text-white/75 tracking-tight font-aeonik font-light">
+                  Tech Consultant, Algiers
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
